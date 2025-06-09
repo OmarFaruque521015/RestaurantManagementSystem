90c6122ae138352f35419a683fc042e72416429e
OM@R <faruqueumar512@gmail.com>
2025-06-10 01:55:14 +0600

    Addition Menu Item and details

--- ResturentManagementSystem/Pages/Customer/Home/Details.cshtml.cs
+++ ResturentManagementSystem/Pages/Customer/Home/Details.cshtml.cs
@@ -13,7 +13,7 @@ namespace RestaurantManagementSystem.Pages.Admin.Customer.Home
         {
             _unitOfWork = unitOfWork;
         }
-
+         
         public MenuItem MenuItem { get; set; }
         [Range(1,100,ErrorMessage ="Please select a count between 1 and 100")]
         public int Count {  get; set; }
